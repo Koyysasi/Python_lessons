ffb7ab5161f8ce018b60bf6586dc447bb70d706e
Koyysasi <koyysasi12@gmail.com>
2024-09-26 18:19:02 +0200

    2024-09-26

---
+++ Scripts/main.py
@@ -0,0 +1,58 @@
+def recursive(num):
+    print(num)
+    if num > 0:
+        num -= 1
+        recursive(num)
+
+
+def fact(n):
+    if n <= 0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+
+def fact2(n, result = 1):
+    if n <= 0:
+        return result
+    else:
+        return fact2(n-1, result * n)
+
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def mathstuff(n, value=0):
+    if n <= 0:
+        return value
+    else:
+        return mathstuff(n-1, value+(1/n))
+
+
+def harm(n):
+    if n <= 1:
+        return 1
+    else:
+        return 1 / n + (harm(n-1))
+
+
+def exponent(a, b):
+    if b == 0:
+        return 1
+    elif a == 0:
+        return 0
+    elif b == 1:
+        return a
+    else:
+        return exponent(a, b-1) * a
+
+
+
+if __name__ == "__main__":
+    print(exponent(10, 10))
