f6c9fcf2cbb5bed2e15cf83d72b36e597a61256f
Koyysasi <koyysasi12@gmail.com>
2024-10-03 18:35:23 +0200

    2024-09-26

---
+++ Scripts/package.py
@@ -0,0 +1,34 @@
+import math
+import cmath
+import random
+import matplotlib.pyplot as plt
+
+
+def main():
+	print(-math.inf)
+	print(cmath.sqrt(-1))
+	print(math.floor(random.random()*1000))
+
+
+def poloska():
+	values = [4139974, 93026]
+	labels = ["Az Eu. unió poloskák befolyása alatt lévő része", "Nem elfoglalt területek"]
+	plt.pie(values, labels=labels, autopct='%.2f%%')
+	plt.title("Poloska területek")
+	plt.show()
+
+
+def random1():
+	value = []
+	for i in range(10000000):
+		j = random.randint(0,4)
+		value.append(j)
+	ratio = []
+	for i in range(5):
+		ratio.append(value.count(i))
+	plt.pie(ratio, autopct='%.2f%%')
+	plt.show()
+
+
+if __name__ == "__main__":
+	poloska()
